7553e5af40fac18a0e4c07a3622dc24e3a72fe31
CondoriM <condoria.miguel@gmail.com>
2022-03-30 15:21:24 +0200

    correction snack4

--- assets/snack4.js
+++ assets/snack4.js
@@ -1,19 +1,12 @@
 const invitati = ['marco','paolo','chiara'];
 const nome = prompt('inserisci il tuo nome');
 
-Boolean vero = false;
 for(let i = 0; i < Array.length; i++){
     ammessi = invitati[i];
 
     if(nome === ammessi){
-        Boolean vero = true;
-        console.log(vero)
+        console.log('permesso consentito');
+    } else{
+        console.log('permesso vietato');
     }
-}
-
-if(boolean = false){
-    alert('Non hai il permesso di entrare')
-} else{
-    alert('entri pure');
-    Boolean vero = false;
 }
