f6b3bf525ce747aed83a74b494041a4cdb1b0806
CondoriM <condoria.miguel@gmail.com>
2022-03-31 11:44:55 +0200

    completed snack 3 while

---
+++ assets3.0/snack3.js
@@ -0,0 +1,11 @@
+let i = 0;
+let somma = 0; 
+
+while(i < 10){
+    num = Number(prompt('inserisci un numero'));
+    somma = somma + num;
+
+    console.log(somma);
+    i++
+}
+console.log(`il risultato è = ${somma}`);
